72696b8c5c09a2af51207b5c56df2e5849675181
Valerie Gadapati <valerie.gadapati@gmail.com>
2020-09-26 16:53:58 -0400

    week 5: arrays and start api proj

---
+++ APITest/src/main/java/App.java
@@ -0,0 +1,12 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) {
+    }
+}
