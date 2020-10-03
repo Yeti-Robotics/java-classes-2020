b33a020e07615e84a5c2342bcc688eadeba71abe
Valerie Gadapati <valerie.gadapati@gmail.com>
2020-10-03 19:33:36 -0400

    week 6

--- APITest/src/main/java/App.java
+++ APITest/src/main/java/App.java
@@ -8,5 +8,26 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Response response = null;
+
+        try {
+            response = okHttpClient.newCall(new Request.Builder()
+                    .get()
+                    .url("https://api.icndb.com/jokes/random")
+                    .build()).execute();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = null;
+
+        try {
+            jsonNode = objectMapper.readTree(response.body().string());
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(jsonNode.get("value").get("joke"));
     }
 }
